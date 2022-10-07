79060610ba2d3003b798bca02ea7d4ae6863b99c
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2022-10-08 04:49:14 +0530

    dashboard

--- app/src/main/java/com/ryan/project/mainactivity/mainfragments/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/ryan/project/mainactivity/mainfragments/ui/dashboard/DashboardFragment.kt
@@ -1,10 +1,12 @@
 package com.ryan.project.mainactivity.mainfragments.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.ryan.project.R
 import com.ryan.project.databinding.FragmentDashboardBinding
+import com.ryan.project.service.LocationService
 
 class DashboardFragment: Fragment(R.layout.fragment_dashboard) {
     private lateinit var binding: FragmentDashboardBinding
@@ -12,5 +14,24 @@ class DashboardFragment: Fragment(R.layout.fragment_dashboard) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDashboardBinding.bind(view)
+
+        binding.apply {
+            markEntryDashoard.setOnClickListener {
+                Intent(requireContext(), LocationService::class.java).also {
+                    requireContext().startService(it)
+                }
+            }
+            markExitDashboard.setOnClickListener {
+                Intent(requireContext(), LocationService::class.java).also {
+                    requireContext().stopService(it)
+                }
+            }
+            manageLeaveDashboard.setOnClickListener {
+
+            }
+            fieldWorkDashboard.setOnClickListener {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/ryan/project/service/LocationService.kt
+++ app/src/main/java/com/ryan/project/service/LocationService.kt
@@ -1,20 +1,81 @@
 package com.ryan.project.service
 
+import android.Manifest
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.ryan.project.utils.Constants
+import com.ryan.project.utils.Constants.CENTER_LATITUDE
+import com.ryan.project.utils.Constants.CENTER_LONGITUDE
 import com.ryan.project.utils.Constants.LOCATION_SERVICE_TAG
+import javax.inject.Inject
 
-class LocationService: Service() {
+class LocationService : Service() {
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val dataString = intent?.getStringExtra("ExtraData")
-        dataString?.let {
-            Log.d(LOCATION_SERVICE_TAG, dataString)
+
+        val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(this, "You are not in campus", Toast.LENGTH_SHORT).show()
+        } else {
+            val locationListener = object : LocationListener {
+                override fun onLocationChanged(location: Location) {
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    val accuracy = location.accuracy
+
+
+                    val results = FloatArray(1)
+
+
+                    Location.distanceBetween(
+                        CENTER_LATITUDE, CENTER_LONGITUDE, latitude, longitude, results
+                    )
+                    val distanceInMeters = results[0]
+                    val inCampus = distanceInMeters < Constants.ALLOWED_RADIUS
+
+                    if (inCampus) {
+                        Toast.makeText(
+                            this@LocationService,
+                            "Your entry is marked!!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            this@LocationService,
+                            "You are not in campus",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+                override fun onProviderEnabled(provider: String) {}
+                override fun onProviderDisabled(provider: String) {}
+            }
+
+            try {
+                locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener
+                )
+            } catch (e: Exception) {
+                Log.d("TAG_ERROR", "getLocation: ${e.message}")
+            }
         }
         return START_STICKY
     }

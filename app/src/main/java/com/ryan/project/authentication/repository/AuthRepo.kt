ffa70339ba5220b71d3f397b0a907b613d014afe
JayPatel2809 <pateljay280901@gmail.com>
2022-10-07 14:32:53 +0530

    Auth repo functions added

--- app/src/main/java/com/ryan/project/authentication/repository/AuthRepo.kt
+++ app/src/main/java/com/ryan/project/authentication/repository/AuthRepo.kt
@@ -1,4 +1,14 @@
 package com.ryan.project.authentication.repository
 
+import com.ryan.project.entity.Employee
+import com.ryan.project.utils.Resource
+
 interface AuthRepo {
+        suspend fun register(email: String, username: String, password: String): Resource<Employee>
+
+        suspend fun login(email: String, password: String): Resource<Employee>
+
+        suspend fun forgotPassword(email: String): Resource<Boolean>
+
+        suspend fun changePassword(email: String, oldPassword: String, newPassword: String): Resource<Boolean>
 }

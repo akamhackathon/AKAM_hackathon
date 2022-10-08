08557d09f5e7eac9810de9a74c366210bf107ac9
JayPatel2809 <pateljay280901@gmail.com>
2022-10-08 14:33:18 +0530

    Admin Dashboard fragment created

--- app/src/main/java/com/ryan/project/mainactivityadmin/mainfragments/ui/admindashboard/FragmentAdminDashboard.kt
+++ app/src/main/java/com/ryan/project/mainactivityadmin/mainfragments/ui/admindashboard/FragmentAdminDashboard.kt
@@ -3,6 +3,7 @@ package com.ryan.project.mainactivityadmin.mainfragments.ui.admindashboard
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ryan.project.R
 import com.ryan.project.databinding.FragmentAdminDashboardBinding
 
@@ -14,6 +15,31 @@ class FragmentAdminDashboard: Fragment(R.layout.fragment_admin_dashboard) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentAdminDashboardBinding.bind(view)
+        binding.apply {
+            addEpTv.setOnClickListener {
+                findNavController().navigate(R.id.registerFragment)
+            }
+
+            requestSheetTv.setOnClickListener {
+                // TODO: API CALL
+            }
+
+            leaveReqTv.setOnClickListener {
+                findNavController().navigate(R.id.applyLeaveFragment)
+            }
+
+            onFieldAttendanceTv.setOnClickListener {
+                // TODO: On Field attedndance
+            }
+
+            overtimeDetailTv.setOnClickListener {
+                // TODO: On Overtime Detail
+            }
+            employeeListTv.setOnClickListener {
+                // TODO: On employee List
+            }
+
+        }
 
     }
 
